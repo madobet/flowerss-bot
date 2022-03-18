c356630b923977cbe458236cb0917f7f047127c2
madobet <madobet@outlook.com>
2022-03-18 15:37:39 +0800

    甘雨的初次见面语音

--- internal/bot/handler/start.go
+++ internal/bot/handler/start.go
@@ -21,13 +21,13 @@ func (s *Start) Command() string {
 }
 
 func (s *Start) Description() string {
-	return "开始使用"
+	return "初次见面"
 }
 
 func (s *Start) Handle(ctx tb.Context) error {
 	user, _ := model.FindOrCreateUserByTelegramID(ctx.Chat().ID)
 	zap.S().Infof("/start user_id: %d telegram_id: %d", user.ID, user.TelegramID)
-	return ctx.Send(fmt.Sprintf("你好，欢迎使用flowerss。"))
+	return ctx.Send(fmt.Sprintf("我明白了，外派工作的「契约」已经拟好了，请过目，即日起就可以为你…欸！名字没有签吗？请让我看一下。唔…名字是甘，甘雨…呼，补上了。话，话说回来，工作具体是指哪些呢？"))
 }
 
 func (s *Start) Middlewares() []tb.MiddlewareFunc {
